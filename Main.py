49af8655f54fb4ed896df35b7d44096eb4db79ad
Danielkim002 <danielskim02dsk@gmail.com>
2022-02-02 18:15:19 -0800

    set up game class

--- Main.py
+++ Main.py
@@ -1,42 +1,48 @@
-game_matrix = ([0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0])
-game_over = False
-def find_largest_number(matrix):
-    max = matrix[0][0]
-    for i in matrix:#i is array
-        for u in i:#u is number in each array
-            if u > max:
-                max = u
-    return max
+class Game:
+    game_matrix = ([0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0])
+    game_over = False
 
-def print_game_state(matrix):
-    largest_digits = len(str(find_largest_number(matrix)))
-    spaces = 0
-    row = ""
-    for i in matrix:
+    def find_largest_number(matrix):
+        max = matrix[0][0]
+        for i in matrix: #i is array
+            for u in i:#u is number in each array
+                if u > max:
+                    max = u
+        return max
 
-        for u in i:
-            row +="["
-            spaces = largest_digits-len(str(u))
-            row += str(u)
-            for x in range(0,spaces):
-                row += " "
-            row += "] "
-        row +="\n"
-    print(row)
+    def print_game_state(matrix):
+        largest_digits = len(str(find_largest_number(matrix)))
+        spaces = 0
+        row = ""
+        for i in matrix:
 
-def new_block(matrix):
-    None
-def player_turn(string,matrix):
-    None
-def player_move():
-    #returns player move as a string making sure it is valid
-    None
-def check_game_state(matrix):
-    #checks if the game is over, if it isnt over, return False
-    None
-    
-while not game_over:
+            for u in i:
+                row +="["
+                spaces = largest_digits-len(str(u))
+                row += str(u)
+                for x in range(0,spaces):
+                    row += " "
+                row += "] "
+            row +="\n"
+        print(row)
+
+    def new_block():
+        #finds which blocks are empty and randomly selects one to fill with either a 2 or a 4
+        #if cant add a block, keep game matrix the same
+        None
+
+    def player_turn():
+        #implements player move and moves board accordingly
+        None
+
+    def check_game_state():
+        #checks if the game is over, if it isnt over, return False
+        None
+
     game_matrix = new_block(game_matrix)
-    print_game_state(game_matrix)
-    player_turn(player_move(),game_matrix)
-    game_over = check_game_state(game_matrix)
+    while not game_over:
+        
+        print_game_state()
+        player_turn()
+        new_block()
+        game_over = check_game_state()
