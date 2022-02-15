5c9c58a8a25f2ec39e20836b9b17b92fea171931
Justin <justinmolina34@gmail.com>
2022-02-15 00:29:25 -0800

    Added two move methods called moveLeft and moveRight. Also performed a huge overhaul to the main file to fix all current bugs. Merged check_game_state and new_block to the main file.

--- Main.py
+++ Main.py
@@ -1,48 +1,162 @@
+import random
+import time
+from random import randrange
+
 class Game:
-    game_matrix = ([0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0])
+    game_matrix = ([0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0])
     game_over = False
 
-    def find_largest_number(matrix):
+    def find_largest_number(self, matrix):
         max = matrix[0][0]
-        for i in matrix: #i is array
-            for u in i:#u is number in each array
+        for i in matrix:  # i is array
+            for u in i:  # u is number in each array
                 if u > max:
                     max = u
         return max
 
-    def print_game_state(matrix):
-        largest_digits = len(str(find_largest_number(matrix)))
+    def print_game_state(self, matrix):
+        largest_digits = len(str(self.find_largest_number(matrix)))
         spaces = 0
         row = ""
         for i in matrix:
-
             for u in i:
-                row +="["
-                spaces = largest_digits-len(str(u))
+                row += "["
+                spaces = largest_digits - len(str(u))
                 row += str(u)
-                for x in range(0,spaces):
+                for x in range(0, spaces):
                     row += " "
                 row += "] "
-            row +="\n"
+            row += "\n"
         print(row)
 
-    def new_block():
-        #finds which blocks are empty and randomly selects one to fill with either a 2 or a 4
-        #if cant add a block, keep game matrix the same
+    def new_block(self, mat):
+        emptySpace = len(mat) * len(mat[0])
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if (mat[i][j] != 0):
+                    emptySpace -= 1
+        if emptySpace == 0: return mat
+        while True:
+            valueLoc = random.randint(1, len(mat) * len(mat[0]))
+            for x in range(len(mat)):
+                for y in range(len(mat[0])):
+                    if valueLoc == (x * 4) + (y + 1) and mat[x][y] != 0:
+                        valueLoc += 1
+                    elif valueLoc == (x * 4) + (y + 1):
+                        # mat[x][y] = random.randint(1, 2) * 2
+                        # Commenting it out as 2 for now
+                        mat[x][y] = 2
+                        return mat
+
+    def moveRowLeft(self, row):
+        # Step 1: Get the value at the left-most position and save its position
+        pos = 0
+        val_at_pos = self.game_matrix[row][pos]
+        # Step 2: Iterate from left to right of the row
+        for index in range(4):
+            # Step 3: Get the value at the current index
+            val_at_index = self.game_matrix[row][index]
+            # Ignore all cases where value at index is 0, and the beginning case
+            if index != pos and val_at_index != 0:
+                # Step 5: Check all possible cases:
+                #   If value at position is equal to value at index
+                #   If value at position is equal to 0
+                #   If value at position is not equal to value at index
+
+                # Case when value at position value is equal to value at index
+                # Multiplies the value at position by two and deletes value at index
+                # Shifts position by one to the right and sets new value at position
+                if val_at_index == val_at_pos:
+                    self.game_matrix[row][pos] *= 2
+                    self.game_matrix[row][index] = 0
+                    pos += 1
+                    val_at_pos = self.game_matrix[row][pos]
+                # Case when value at position is equal to 0
+                # Sets value at position to the value at index
+                # Deletes value at index
+                # Shifts position by one the right and sets new value at position
+                elif val_at_pos == 0:
+                    self.game_matrix[row][pos] = val_at_index
+                    self.game_matrix[row][index] = 0
+                    val_at_pos = self.game_matrix[row][pos]
+                # Case when value at position is not equal to value at position
+                # Shifts position by one to the right and sets new value at position
+                # Checks if new value at position is not 0
+                #   If new value position is 0,
+                #       Sets value at new position to the value at index
+                #       Deletes value at index
+                else:
+                    pos += 1
+                    val_at_pos = self.game_matrix[row][pos]
+                    if val_at_pos == 0:
+                        self.game_matrix[row][pos] = val_at_index
+                        self.game_matrix[row][index] = 0
+                        val_at_pos = self.game_matrix[row][pos]
+
+    def moveRowRight(self, row):
+        pos = 3
+        val_at_pos = self.game_matrix[row][pos]
+        for index in range(4):
+            val_at_index = self.game_matrix[row][3-index]
+            if (3-index) != pos and val_at_index != 0:
+                if val_at_index == val_at_pos:
+                    self.game_matrix[row][pos] *= 2
+                    self.game_matrix[row][3-index] = 0
+                    pos -= 1
+                    val_at_pos = self.game_matrix[row][pos]
+                elif val_at_pos == 0:
+                    self.game_matrix[row][pos] = val_at_index
+                    self.game_matrix[row][3-index] = 0
+                    val_at_pos = self.game_matrix[row][pos]
+                else:
+                    pos -= 1
+                    val_at_pos = self.game_matrix[row][pos]
+                    if val_at_pos == 0:
+                        self.game_matrix[row][pos] = val_at_index
+                        self.game_matrix[row][3-index] = 0
+                        val_at_pos = self.game_matrix[row][pos]
+
+    def player_turn(self, input):
+        # Move values around the board depending on given input
+        # Follows the game rules of 2048, will only add numbers if the neighboring value is the same value
+        # If value are not the same, stop digit from moving further in that direction
+        # Assume that 0 is an empty space and allow free movement on these spots
+
+        # Assume that the board size is always 4x4
+
+        if input == 0:
+            for row in range(4):
+                Game.moveRowLeft(self, row)
+            print("Left")
+        elif input == 1:
+            for row in range(4):
+                Game.moveRowRight(self, row)
+            print("Right")
+
+        # implements player move and moves board accordingly
         None
 
-    def player_turn():
-        #implements player move and moves board accordingly
+    def check_game_state(self, mat):
+        if self.player_turn("up", mat) == mat and self.player_turn("down", mat) == mat and self.player_turn("left",
+                                                                                                            mat) == mat and self.player_turn(
+                "right", mat) == mat:
+            return False
+        return True
         None
 
-    def check_game_state():
-        #checks if the game is over, if it isnt over, return False
-        None
+    def __init__(self):
+        game_matrix = self.new_block(self.game_matrix)
+        game_matrix = self.new_block(self.game_matrix)
+        while not self.game_over:
+            self.print_game_state(self.game_matrix)
+            t = randrange(0, 2)
+            print(t)
+            self.player_turn(t)
+            self.new_block(self.game_matrix)
+            time.sleep(0.5)
+            # game_over = self.check_game_state()
 
-    game_matrix = new_block(game_matrix)
-    while not game_over:
-        
-        print_game_state()
-        player_turn()
-        new_block()
-        game_over = check_game_state()
+    pass
+
+
+game = Game()
