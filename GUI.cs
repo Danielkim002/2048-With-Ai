5f93d14253cc9d2ed574312e53f9a4797ff5bdec
JustinM3434 <justinmolina34@gmail.com>
2022-02-17 14:21:14 -0800

    Initial commit

--- GUI.cs
+++ GUI.cs
@@ -4,14 +4,17 @@ using System.Drawing;
 
 class GUI : Form {
 
+    Color BackgroundColor = Color.FromArgb(255, 248, 230);
+    String TitleName = "2048 With Machine Learning";
+    Size MinSize = new Size(720, 480);
     public GUI() {
-        this.Name = "Example";
-        this.MinimumSize = new Size(720, 480);
+        this.BackColor = BackgroundColor;
+        this.Text = TitleName;
+        this.MinimumSize = MinSize;
     }
 }
 
 class Program {
-
     public static void Main(String[] args) {
         GUI gui = new GUI();
         Application.Run(gui);
